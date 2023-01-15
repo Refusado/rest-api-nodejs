df1144cb47db6d8f350b7a9765d565c584519b01
Renan <orefusado@gmail.com>
2023-01-15 05:15:38 -0300

    feat: implementando o método dele + melhorias

--- controllers/PersonController.js
+++ controllers/PersonController.js
@@ -22,9 +22,7 @@ class PersonController {
       
       return response.status(201).json({ message: `${personData.name} criado com sucesso.` });
     } catch (error) {
-      return response.status(500).json({
-         error: error.message
-      });
+      return response.status(500).json({ error: error.message });
     }
   }
 
@@ -45,9 +43,7 @@ class PersonController {
           return response.status(200).json(people);
       }
     } catch (error) {
-      return response.status(500).json({
-        error: error.message
-      });
+      return response.status(500).json({ error: error.message });
     }
   }
 
@@ -62,18 +58,33 @@ class PersonController {
         approved
       };
 
-      const updatedPerson = await Person.updateOne( {_id: id }, personData);
+      const update = await Person.updateOne( {_id: id }, personData);
 
-      if (updatedPerson.matchedCount) {
+      if (update.matchedCount) {
+        const updatedPerson = await Person.findOne({ _id: id });
         return response.status(200).json(updatedPerson);
       }
         
-      return response.status(422).json({ message: `Atualização não realizada` });
+      return response.status(422).json({ message: 'Atualização malsucedida' });
 
     } catch (error) {
-      return response.status(500).json({
-        error: error.message
-      });
+      return response.status(500).json({ error: error.message });
+    }
+  }
+
+  async delete(request, response) {
+    const id = request.params.id;
+    const person = await Person.findOne({ _id: id });
+
+    if (person) {
+      try {
+        const deletedPerson = await Person.deleteOne({ _id: id });
+        response.status(200).json(deletedPerson);
+      } catch (error) {
+        response.status(500).json({ message: error.message });
+      }
+    } else {
+      return response.status(422).json({ message: `ID ${id} não encontrado` });
     }
   }
 }
--- routes/person.routes.js
+++ routes/person.routes.js
@@ -6,5 +6,6 @@ router.post('/person', PersonController.create);
 router.get('/person', PersonController.read);
 router.get('/person/:id', PersonController.read);
 router.patch('/person/:id', PersonController.update);
+router.delete('/person/:id', PersonController.delete);
 
 module.exports = router;
